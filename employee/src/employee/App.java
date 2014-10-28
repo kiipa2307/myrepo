b0e70027082a90c6b87f1c4c1ff796561d53facf
kiipa2307 <dana@172.29.219.69>
2014-10-28 10:44:34 +0100

    myrepo - add first files

---
+++ employee/src/employee/App.java
@@ -0,0 +1,17 @@
+package employee;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Employee emp1 = new Employee();
+		
+		emp1.firstName = "Peter";
+		emp1.lastName = "MŸller";
+		
+		emp1.printfullName("Peter", "MŸller");
+		
+
+	}
+
+}
---
+++ employee/src/employee/Employee.java
@@ -0,0 +1,21 @@
+package employee;
+
+public class Employee {
+
+	String firstName;
+	String lastName;
+	Integer ID;
+	
+	void printfullName(String first, String last) {
+		
+//		f = firstName;
+//		l = lastName;
+		
+//		firstName = f;
+//		lastName = l;
+		
+		System.out.println(first + " " + last);
+	}
+	
+	
+}
